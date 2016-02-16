be6dd5c0865126f0cda40cf0624454d24e06f75c
System Administrator <root@admindeMacBook-Pro.local>
2016-02-16 14:28:24 +0800

    add page

--- CMDB/views.py
+++ CMDB/views.py
@@ -12,14 +12,14 @@ from django.core.paginator import Paginator,EmptyPage,PageNotAnInteger
 #     return render(request, 'test.html', {'hosts': hosts})
 def test(request):
     hosts = Hosts.objects.all() #查出所有hosts表的数据
-    paginator = Paginator(hosts, 50) #每页显示50个
-    page = request.GET.get('page')
+    paginator = Paginator(hosts, 50) #每页显示50个,分页器,实例化一个分页对象
+    page = request.GET.get('page') #html传递参数page
     try :
-        hosts_list = paginator.page(page)
-    except PageNotAnInteger :
-        hosts_list = paginator.page(1)
-    except EmptyPage :
-        hosts_list = paginator.paginator(paginator.num_pages)
+        hosts_list = paginator.page(page) #当前需要显示的页
+    except PageNotAnInteger : # 如果页码不是个整数
+        hosts_list = paginator.page(1)  # 取第一页的记录
+    except EmptyPage : # 如果页码太大，没有相应的记录
+        hosts_list = paginator.paginator(paginator.num_pages) # 取最后一页的记录
     return render(request, 'test.html', {'hosts_list' : hosts_list})
 
 def boot(request):
@@ -33,7 +33,7 @@ def search(request):
     if 'q' in request.GET: #有数据提交进来，q是变量名称
         q = request.GET['q']
         hosts = Hosts.objects.all().filter(hostname__icontains = q)
-        paginator = Paginator(hosts, 100) #每页显示100个
+        paginator = Paginator(hosts, 200) #每页显示100个
         page = request.GET.get('page')
         try :
             hosts_list = paginator.page(page)
